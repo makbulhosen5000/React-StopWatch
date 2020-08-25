60a85b76702b2a15e22d0e63cf875b4b7bc46672
mhakash5000 <mhakash5000@gmail.com>
2020-08-25 08:31:34 +0600

    react commit

---
+++ src/Components/Title/Title.js
@@ -0,0 +1,67 @@
+import React,{Component} from 'react';
+import './Title.css';
+class Title extends Component{
+     constructor(props){
+         super(props)
+        this.state={
+            title:'CodingDuck StopWatch',
+            isInput:false
+        }
+         
+     }
+     editHandler(){
+         this.setState({
+             ...this.state,
+             isInput:true
+         })
+     }
+     inputChange(event){
+         this.setState({
+             ...this.state,
+             title:event.target.value
+         })
+     }
+     onKeyHandler(event){
+         if(event.key==='Enter'){
+             this.setState({
+                 ...this.state,
+                 isInput:false
+             })
+         }
+     }
+     onBlurHandler(event){
+        this.setState({
+            ...this.state,
+            isInput:false
+        })
+     }
+    render(){
+        let output=null
+        if(this.state.isInput){
+            output=(
+                <div className="Title">
+                    <input 
+                     onChange={(event)=>this.inputChange(event)}
+                     onKeyPress={(event)=>this.onKeyHandler(event)}
+                     onBlur={(event)=>this.onBlurHandler(event)}
+                      className="form-control" type="text"
+                     value={this.state.title}/>
+                </div>
+            )
+        }else{
+            output=(
+                <div className="d-flex Title">
+                    <h1 className="display-4">{this.state.title}</h1>
+                    <span onClick={()=>this.editHandler()} className="ml-auto edit-icon">
+                         <i className="fas fa-pencil-alt"></i>
+                     </span>
+                </div>
+            )
+        }
+        return(
+         
+        <div>{output}</div>
+        )
+    }
+}
+export default Title;
